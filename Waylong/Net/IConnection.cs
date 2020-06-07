a6e77c79f5644c625ee5b1d7ccc89fb60b421b11
Waylon <1664940243LWY@gmail.com>
2020-06-07 19:10:29 +0800

    Add : TCPConnection

--- Waylong/Net/IConnection.cs
+++ Waylong/Net/IConnection.cs
@@ -3,22 +3,42 @@ using System.Net.Sockets;
 
 namespace Waylong.Net {
 
+    public enum NetworkMode {
+        None,       //未知
+        Connect,    //接入
+        Listen,     //監聽
+    }
+
     public interface IConnection {
 
+        #region Property
+
         //連線目標IP
-        string GetIP { get; }
+        string IP { get; }
 
         //連線目標端口
-        int GetPort { get; }
+        int Port { get; }
 
         //連線目標地址
-        //string IPAddress { get => IP + ":" + Port.ToString(); }
+        string IPAddress { get; }
+
+        //連線模式
+        NetworkMode NetworkMode { get; }
 
         //連線所使用的協議
-        ProtocolType GetProtocolType { get; }
+        ProtocolType ProtocolType { get; }
 
-        //連線方法
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 建立連線
+        /// </summary>
+        /// <returns>連線是否成功</returns>
         bool Connection();
+
+        #endregion
     }
 
 }
